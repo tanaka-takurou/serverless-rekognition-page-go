437b3439be7cc069e2a278bcdfba803d89a4b60b
tanaka.takurou <tanaka.takurou@icloud.com>
2020-08-18 16:23:26 +0900

    fix README.md

--- api/main.go
+++ api/main.go
@@ -22,7 +22,6 @@ type APIResponse struct {
 type Response events.APIGatewayProxyResponse
 
 const layout       string = "2006-01-02 15:04"
-const languageCode string = "ja"
 
 func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (Response, error) {
 	var jsonBytes []byte
